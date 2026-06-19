7c9006b7cde9536cb13886752c3c6fab869be94b
gaelc08 <gaelc08@users.noreply.github.com>
2026-06-20 00:22:45 +0200

    style: sidebar dark JCC theme + fix position fixe desktop

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
-const CACHE_VERSION = 'judo-coach-pwa-v118';
-const APP_BUILD_ID = '2026-06-20-r02';
+const CACHE_VERSION = 'judo-coach-pwa-v119';
+const APP_BUILD_ID = '2026-06-20-r03';
 const BASE_PATH = new URL('./', self.location.href).pathname;
 const INDEX_URL = `${BASE_PATH}index.html`;
 const OFFLINE_URL = `${BASE_PATH}offline.html`;
