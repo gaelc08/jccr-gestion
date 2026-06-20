589c658b48e19928f648df5eb6365707ac660136
gaelc08 <gaelc08@users.noreply.github.com>
2026-06-20 10:10:08 +0200

    chore: bump app version to r12

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
-const CACHE_VERSION = 'judo-coach-pwa-v127';
-const APP_BUILD_ID = '2026-06-20-r11';
+const CACHE_VERSION = 'judo-coach-pwa-v128';
+const APP_BUILD_ID = '2026-06-20-r12';
 const BASE_PATH = new URL('./', self.location.href).pathname;
 const INDEX_URL = `${BASE_PATH}index.html`;
 const OFFLINE_URL = `${BASE_PATH}offline.html`;
