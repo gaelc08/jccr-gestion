c8cd89b55751aedd6986163e2fa2450a3bd207a6
gaelc08 <gaelc08@users.noreply.github.com>
2026-07-06 16:01:22 +0200

    chore: bump app version to 2026-07-06-r01

--- public/modules/env.js
+++ public/modules/env.js
@@ -43,8 +43,8 @@ export const supabaseKey = effectiveEnv === 'dev'
   ? (localDevKeyOverride || DEV_SUPABASE_KEY || PROD_SUPABASE_KEY)
   : PROD_SUPABASE_KEY;
 
-export const VERSION_DATE = '2026-06-20';
-export const VERSION_INCREMENT = '14';
+export const VERSION_DATE = '2026-07-06';
+export const VERSION_INCREMENT = '01';
 export const BUILD_ID = `${VERSION_DATE}-r${VERSION_INCREMENT}`;
 
 if (effectiveEnv === 'dev' && !localDevKeyOverride) {
